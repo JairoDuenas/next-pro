59df9389c543dc3011b006b3f93ea1a3a38a4a28
JairoDuenas <jairoduenas.ing@gmail.com>
2022-05-18 09:10:58 -0500

    Guardando token en cookie para mantener sesion

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -24,11 +24,20 @@ function useProvideAuth() {
         "Content-Type": "application/json",
       },
     };
-    const {
-      data: { access_token },
-    } = await axios.post(endPoints.auth.login, { email, password }, options);
-    console.log(access_token);
+    const { data: access_token } = await axios.post(
+      endPoints.auth.login,
+      { email, password },
+      options
+    );
+    if (access_token) {
+      const token = access_token.access_token;
+      Cookie.set("token", token, { expires: 5 });
+      axios.defaults.headers.Authorization = `Bearer ${token}`;
+      const { data: user } = await axios.get(endPoints.auth.profile);
+      setUser(user);
+    }
   };
+
   return {
     user,
     signIn,
