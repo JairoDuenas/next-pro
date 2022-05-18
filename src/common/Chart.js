5d630568170876975a2bd3de456cfd78483143b4
JairoDuenas <jairoduenas.ing@gmail.com>
2022-05-18 11:07:25 -0500

    Creando el componente Chart

---
+++ src/common/Chart.js
@@ -0,0 +1,41 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chartjs";
+
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const Chart = ({ chartData }) => {
+  return (
+    <>
+      <Bar
+        data={chartData}
+        options={{
+          title: {
+            display: true,
+            text: "Category",
+            fontsize: 20,
+          },
+          legend: {
+            display: true,
+            position: "right",
+          },
+        }}
+      />
+    </>
+  );
+};
