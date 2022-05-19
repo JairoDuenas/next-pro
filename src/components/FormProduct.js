47a8589699e357ecb8c3178b30a739ae849ace77
JairoDuenas <jairoduenas.ing@gmail.com>
2022-05-19 08:37:33 -0500

    Careación y cargado de datos para actualizar un producto

--- src/components/FormProduct.js
+++ src/components/FormProduct.js
@@ -1,25 +1,24 @@
-import { useRef } from "react";
-import { addProduct } from "@services/api/product";
+import { useRef } from 'react';
+import { addProduct } from '@services/api/product';
 
-export default function FormProduct({ setOpen, setAlert }) {
+export default function FormProduct({ setOpen, setAlert, product }) {
   const formRef = useRef(null);
-
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(formRef.current);
     const data = {
-      title: formData.get("title"),
-      price: parseInt(formData.get("price")),
-      description: formData.get("description"),
-      categoryId: parseInt(formData.get("category")),
-      images: [formData.get("images").name],
+      title: formData.get('title'),
+      price: parseInt(formData.get('price')),
+      description: formData.get('description'),
+      categoryId: parseInt(formData.get('category')),
+      images: [formData.get('images').name],
     };
     addProduct(data)
       .then(() => {
         setAlert({
           active: true,
-          message: "Product added successfully",
-          type: "success",
+          message: 'Product added successfully',
+          type: 'success',
           autoClose: false,
         });
         setOpen(false);
@@ -28,7 +27,7 @@ export default function FormProduct({ setOpen, setAlert }) {
         setAlert({
           active: true,
           message: error.message,
-          type: "error",
+          type: 'error',
           autoClose: false,
         });
       });
@@ -40,13 +39,11 @@ export default function FormProduct({ setOpen, setAlert }) {
         <div className="px-4 py-5 bg-white sm:p-6">
           <div className="grid grid-cols-6 gap-6">
             <div className="col-span-6 sm:col-span-3">
-              <label
-                htmlFor="title"
-                className="block text-sm font-medium text-gray-700"
-              >
+              <label htmlFor="title" className="block text-sm font-medium text-gray-700">
                 Title
               </label>
               <input
+                defaultValue={product?.title}
                 type="text"
                 name="title"
                 id="title"
@@ -54,13 +51,11 @@ export default function FormProduct({ setOpen, setAlert }) {
               />
             </div>
             <div className="col-span-6 sm:col-span-3">
-              <label
-                htmlFor="price"
-                className="block text-sm font-medium text-gray-700"
-              >
+              <label htmlFor="price" className="block text-sm font-medium text-gray-700">
                 Price
               </label>
               <input
+                defaultValue={product?.price}
                 type="number"
                 name="price"
                 id="price"
@@ -68,15 +63,13 @@ export default function FormProduct({ setOpen, setAlert }) {
               />
             </div>
             <div className="col-span-6">
-              <label
-                htmlFor="category"
-                className="block text-sm font-medium text-gray-700"
-              >
+              <label htmlFor="category" className="block text-sm font-medium text-gray-700">
                 Category
               </label>
               <select
                 id="category"
                 name="category"
+                defaultValue={product?.category}
                 autoComplete="category-name"
                 className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               >
@@ -89,13 +82,11 @@ export default function FormProduct({ setOpen, setAlert }) {
             </div>
 
             <div className="col-span-6">
-              <label
-                htmlFor="description"
-                className="block text-sm font-medium text-gray-700"
-              >
+              <label htmlFor="description" className="block text-sm font-medium text-gray-700">
                 Description
               </label>
               <textarea
+                defaultValue={product?.description}
                 name="description"
                 id="description"
                 autoComplete="description"
@@ -105,18 +96,10 @@ export default function FormProduct({ setOpen, setAlert }) {
             </div>
             <div className="col-span-6">
               <div>
-                <label className="block text-sm font-medium text-gray-700">
-                  Cover photo
-                </label>
+                <label className="block text-sm font-medium text-gray-700">Cover photo</label>
                 <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                   <div className="space-y-1 text-center">
-                    <svg
-                      className="mx-auto h-12 w-12 text-gray-400"
-                      stroke="currentColor"
-                      fill="none"
-                      viewBox="0 0 48 48"
-                      aria-hidden="true"
-                    >
+                    <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                       <path
                         d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
                         strokeWidth={2}
@@ -130,18 +113,11 @@ export default function FormProduct({ setOpen, setAlert }) {
                         className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                       >
                         <span>Upload a file</span>
-                        <input
-                          id="images"
-                          name="images"
-                          type="file"
-                          className="sr-only"
-                        />
+                        <input defaultValue={product?.images} id="images" name="images" type="file" className="sr-only" />
                       </label>
                       <p className="pl-1">or drag and drop</p>
                     </div>
-                    <p className="text-xs text-gray-500">
-                      PNG, JPG, GIF up to 10MB
-                    </p>
+                    <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
                   </div>
                 </div>
               </div>
